63f01560dc4a37628642c484784c001fd30ce2c0
Richard Weerasinghe <rich.w@me.com>
2023-05-30 23:01:19 +1200

    fix edit tests

--- openai/edits_test.go
+++ openai/edits_test.go
@@ -13,9 +13,9 @@ import (
 func TestCreateEdit(t *testing.T) {
 	testModelID := "testModelID"
 	ts := openai_test.NewTestServer()
-	ts.RegisterHandler("/v1/chat/completions", func(w http.ResponseWriter, _ *http.Request) {
-		resBytes, _ := json.Marshal(openai.CompletionResponse{
-			Model: testModelID,
+	ts.RegisterHandler("/v1/edits", func(w http.ResponseWriter, _ *http.Request) {
+		resBytes, _ := json.Marshal(openai.EditResponse{
+			Created: 1,
 		})
 		fmt.Fprintln(w, string(resBytes))
 	})
@@ -31,7 +31,7 @@ func TestCreateEdit(t *testing.T) {
 	_, err := client.Edits().CreateEdit(&req)
 	t.Helper()
 	if err != nil {
-		t.Error(err, "CreateChatCompletion error")
+		t.Error(err, "CreateEdit error")
 		t.Fail()
 	}
 }
